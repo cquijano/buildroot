497d2247291296289efbe53e5594e0d6abe2c8d7
Carlos Quijano <carlos@crqgestion.es>
2014-05-06 19:31:43 +0200

    added package xdriver_xf86-video-fbturbo

--- package/x11r7/xdriver_xf86-video-fbturbo/xdriver_xf86-video-fbturbo.mk
+++ package/x11r7/xdriver_xf86-video-fbturbo/xdriver_xf86-video-fbturbo.mk
@@ -9,4 +9,8 @@ XDRIVER_XF86_VIDEO_FBTURBO_SITE = git://github.com/ssvb/xf86-video-fbturbo.git
 XDRIVER_XF86_VIDEO_FBTURBO_DEPENDENCIES = xserver_xorg-server xproto_xf86driproto xproto_fontsproto xproto_randrproto xproto_renderproto xproto_videoproto xproto_xproto
 XDRIVER_XF86_VIDEO_FBTURBO_AUTORECONF = YES
 
+define XDRIVER_XF86_VIDEO_FBTURBO_INSTALL_XINIT
+	cp $(BUILD_DIR)/xorg.conf $(TARGET_DIR)/usr/share/X11/xorg.conf.d/01-fbturbo.conf
+endef
+
 $(eval $(autotools-package))
